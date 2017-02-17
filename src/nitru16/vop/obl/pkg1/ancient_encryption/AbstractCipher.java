f34e29c468783fa6e3852ab38ed52e64e42be4d8
NicolaiT <nicolai-1@live.dk>
2017-02-17 15:20:32 +0100

    start

---
+++ src/nitru16/vop/obl/pkg1/ancient_encryption/AbstractCipher.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nitru16.vop.obl.pkg1.ancient_encryption;
+
+/**
+ *
+ * @author Nicolai Truong
+ */
+public abstract class AbstractCipher {
+	protected abstract int findCharIndex(char ch);
+}
